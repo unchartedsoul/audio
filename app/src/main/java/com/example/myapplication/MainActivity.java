30088f8e126c68854fb2597e87f08aa1e7bcd8e6
unchartedsoul <sudanshu144@gmail.com>
2020-06-14 12:52:49 +0530

    upade
    now audio file is added and we can now pause and play

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,13 +1,43 @@
 package com.example.myapplication;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private MediaPlayer mediaPlayer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+         mediaPlayer = MediaPlayer.create(this, R.raw.song);
+
+        Button playButton = (Button)findViewById(R.id.play_button);
+
+        playButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                mediaPlayer.start();
+                Toast.makeText(MainActivity.this, "Play", Toast.LENGTH_SHORT).show();
+            }
+
+        });
+
+
+        Button pauseButton = (Button)findViewById(R.id.pause_button);
+
+        pauseButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                mediaPlayer.pause();
+                Toast.makeText(MainActivity.this, "Pause", Toast.LENGTH_SHORT).show();
+            }
+
+        });
     }
 }
